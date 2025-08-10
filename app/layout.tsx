2bae36809447bcb86487f421832e9a03db69f56e
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-08-10 13:42:37 +0600

    implement signin functionality using Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans} from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 
 const fontSans = FontSans({
@@ -20,6 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+      variables: {
+        colorPrimary: "#3371FF",
+        fontSize: "16px"
+      },
+    }}
+    >
+
     <html lang="en" suppressHydrationWarning>
       <body
           className={cn(
@@ -30,5 +42,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
